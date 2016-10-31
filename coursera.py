4531f86fff85f6d8aca7fa0612e00d5e7fed2e8a
conformist-mw <conformist.mw@gmail.com>
2016-10-31 23:12:11 +0200

    fixed

--- coursera.py
+++ coursera.py
@@ -1,14 +1,14 @@
-import re
-import requests
-from lxml import etree
-from bs4 import BeautifulSoup
-from openpyxl import Workbook
+import argparse
 import json
 import random
-import argparse
+import re
+import requests
+from bs4 import BeautifulSoup
+from lxml import etree
+from openpyxl import Workbook
 
 
-def get_courses_list():
+def obtain_courses_links():
     url = 'https://www.coursera.org/sitemap~www~courses.xml'
     xml = requests.get(url)
     root = etree.fromstring(xml.content)
@@ -17,17 +17,17 @@ def get_courses_list():
     return links[:20]
 
 
-def get_content(url):
+def course_content(url):
     html = requests.get(url).content
     return BeautifulSoup(html, 'lxml')
 
 
-def get_title(soup):
+def course_title(soup):
     title = soup.find('div', {'class': 'title'})
     return title.text if title else None
 
 
-def get_lang(soup):
+def course_lang(soup):
     table = soup.find('table', {'class': 'basic-info-table'})
     trs = table.findAll('tr')
     for tr in trs:
@@ -36,35 +36,35 @@ def get_lang(soup):
             return value.rstrip(',')
 
 
-def get_date(soup):
+def course_start_date(soup):
     script = soup.find('script', {'type': 'application/ld+json'})
     if script and re.search(r'startDate', script.text):
         return json.loads(script.text)['hasCourseInstance'][0]['startDate']
 
 
-def get_weeks(soup):
+def course_duration_in_weeks(soup):
     return len(soup.findAll('div', {'class': 'week'}))
 
 
-def get_rate(soup):
+def course_rate(soup):
     rate = soup.find('div', {'class': 'ratings-text'})
     return rate.text if rate else None
 
 
-def get_course_info(url):
-    soup = get_content(url)
-    course = [
-        get_title(soup),
-        get_lang(soup),
-        get_date(soup),
-        get_weeks(soup),
-        get_rate(soup),
-        url
-    ]
+def collect_course_info(url):
+    soup = course_content(url)
+    course = {
+        'Title': course_title(soup),
+        'Language': course_lang(soup),
+        'Start Date': course_start_date(soup),
+        'Weeks amount': course_duration_in_weeks(soup),
+        'Course Rate': course_rate(soup),
+        'URL': url
+    }
     return course
 
 
-def output_courses_info_to_xlsx(filepath, courses_info):
+def output_courses_info_to_xlsx(filepath, courses):
     headers = ['Title', 'Language', 'Start Date',
                'Weeks amount', 'Course Rate', 'URL']
     wb = Workbook()
@@ -72,15 +72,14 @@ def output_courses_info_to_xlsx(filepath, courses_info):
     ws.title = 'coursera'
     for j, col in enumerate(headers, 1):
         ws.cell(row=1, column=j).value = col
-    for num, course in enumerate(courses_info, 2):
-        for i, val in enumerate(course, 1):
-            if i == 6:
+    for num, course in enumerate(courses, 2):
+        for i, key in enumerate(headers, 1):
+            if key == 'URL':
                 ws.cell(row=num, column=i).value = 'more details →'
-                ws.cell(row=num, column=i).hyperlink = val
+                ws.cell(row=num, column=i).hyperlink = course[key]
             else:
-                ws.cell(row=num, column=i).value = val
+                ws.cell(row=num, column=i).value = course[key]
     wb.save(filename=filepath)
-    print('Данные записаны в файл: {}'.format(filepath))
     return True
 
 
@@ -89,6 +88,9 @@ if __name__ == '__main__':
         description='Скрипт собирает информацию о разных курсах на Курсере')
     parser.add_argument('filepath', help='укажите файл в формате xlsx')
     args = parser.parse_args()
-    links = get_courses_list()
-    lists = [get_course_info(link) for link in links]
-    output_courses_info_to_xlsx(args.filepath, lists)
+    links = obtain_courses_links()
+    courses = [collect_course_info(link) for link in links]
+    if output_courses_info_to_xlsx(args.filepath, courses):
+        print('Data has been written to file: {}'.format(args.filepath))
+    else:
+        print('Something goes wrong :(')
