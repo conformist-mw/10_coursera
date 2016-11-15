ce1d816760927410ddab744cecf3b629a97d9934
conformist-mw <conformist.mw@gmail.com>
2016-11-15 11:13:26 +0200

    renaming funcs

--- coursera.py
+++ coursera.py
@@ -8,26 +8,26 @@ from lxml import etree
 from openpyxl import Workbook
 
 
-def obtain_courses_links():
+def obtain_courses_links(count=20):
     url = 'https://www.coursera.org/sitemap~www~courses.xml'
     xml = requests.get(url)
     root = etree.fromstring(xml.content)
     links = [link.text for link in root.iter('{*}loc') if 'learn' in link.text]
     random.shuffle(links)
-    return links[:20]
+    return links[:count]
 
 
-def course_content(url):
+def fetch_course_page(url):
     html = requests.get(url).content
     return BeautifulSoup(html, 'lxml')
 
 
-def course_title(soup):
+def get_course_title(soup):
     title = soup.find('div', {'class': 'title'})
     return title.text if title else None
 
 
-def course_lang(soup):
+def get_course_lang(soup):
     table = soup.find('table', {'class': 'basic-info-table'})
     trs = table.findAll('tr')
     for tr in trs:
@@ -36,29 +36,29 @@ def course_lang(soup):
             return value.rstrip(',')
 
 
-def course_start_date(soup):
+def get_course_start_date(soup):
     script = soup.find('script', {'type': 'application/ld+json'})
     if script and re.search(r'startDate', script.text):
         return json.loads(script.text)['hasCourseInstance'][0]['startDate']
 
 
-def course_duration_in_weeks(soup):
+def count_course_duration(soup):
     return len(soup.findAll('div', {'class': 'week'}))
 
 
-def course_rate(soup):
+def get_course_rate(soup):
     rate = soup.find('div', {'class': 'ratings-text'})
     return rate.text if rate else None
 
 
 def collect_course_info(url):
-    soup = course_content(url)
+    soup = fetch_course_page(url)
     course = {
-        'Title': course_title(soup),
-        'Language': course_lang(soup),
-        'Start Date': course_start_date(soup),
-        'Weeks amount': course_duration_in_weeks(soup),
-        'Course Rate': course_rate(soup),
+        'Title': get_course_title(soup),
+        'Language': get_course_lang(soup),
+        'Start Date': get_course_start_date(soup),
+        'Weeks amount': count_course_duration(soup),
+        'Course Rate': get_course_rate(soup),
         'URL': url
     }
     return course
@@ -88,7 +88,7 @@ if __name__ == '__main__':
         description='Скрипт собирает информацию о разных курсах на Курсере')
     parser.add_argument('filepath', help='укажите файл в формате xlsx')
     args = parser.parse_args()
-    links = obtain_courses_links()
+    links = obtain_courses_links(20)
     courses = [collect_course_info(link) for link in links]
     if output_courses_info_to_xlsx(args.filepath, courses):
         print('Data has been written to file: {}'.format(args.filepath))
